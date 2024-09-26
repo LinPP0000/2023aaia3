b70fb96b1bc00c8ab5cfbd9c61462b6cc2031181
LinPP0000 <144758472+LinPP0000@users.noreply.github.com>
2024-09-26 16:35:31 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,16 @@
+///week03-1.cpp
+///warning: range-based 'for' loops only available with -std=c++11 or -std=gnu++11
+///CodeBlocks-Settings-Compiler 把 -std=c++11 的 2011年的C++開起來
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    for(char c : s){
+        cout << c << "\n";
+    }
+}
+
