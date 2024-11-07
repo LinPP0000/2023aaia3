991de0b3aa618457c25164a9a571f13bca920017
LinPP0000 <144758472+LinPP0000@users.noreply.github.com>
2024-11-07 16:33:06 +0800

    week09

---
+++ week09/week09-2.c
@@ -0,0 +1,12 @@
+int diagonalSum(int** mat, int matSize, int* matColSize) {
+    int M=matSize;
+        int ans=0;
+        for(int i=0;i<M;i++){
+            ans += mat[i][i];//左上.右下
+            ans += mat[i][M-1-i];//右下.左上
+        }
+        //單數十，要扣掉中間(兩個叉叉的交點)
+        if(M%2==1) ans -= mat[M/2][M/2];
+        return ans;
+    }
+
